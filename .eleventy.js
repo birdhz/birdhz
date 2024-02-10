ce2a825999abcb4e0522b064c42f680fccad12e9
birdhz <birdhz@pm.me>
2024-02-10 00:14:40 -0800

    add custom font and update css

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,18 @@
+// change default build directory from _site to dist
 module.exports = function(eleventyConfig) {
   return {
     dir: {
       output: "dist"
     }
   }
+};
+
+// fonts
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("assets/fonts");
+};
+
+// css
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("bundle.css");
 };
