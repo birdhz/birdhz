f8f5e1333264f4101bd9a8ad3a0f3e7cd0a50571
birdhz <birdhz@pm.me>
2024-02-12 14:30:22 -0800

    add luxon date formatting; add custom filter to sort collections alphabetically on their data.title

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,30 @@
+// Import prior to `module.exports` within `.eleventy.js`
+const { DateTime } = require('luxon');
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addLayoutAlias('base', 'layouts/base.njk')
-  eleventyConfig.addLayoutAlias('page', 'layouts/page.njk')
-  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk')
+  eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
+  eleventyConfig.addLayoutAlias('page', 'layouts/page.njk');
+  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
 
-  eleventyConfig.addPassthroughCopy('./src/css')
-  eleventyConfig.addPassthroughCopy('./src/fonts')
-  eleventyConfig.addPassthroughCopy('./src/img')
+  eleventyConfig.addPassthroughCopy('./src/css');
+  eleventyConfig.addPassthroughCopy('./src/fonts');
+  eleventyConfig.addPassthroughCopy('./src/img');
 
-  eleventyConfig.addWatchTarget('./src/css')
+  eleventyConfig.addWatchTarget('./src/css');
 
-  eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`)
+  eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
+
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toFormat('yyyy.LL.dd');
+    // return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    // return DateTime.fromJSDate(dateObj).toLocaleString({ month: 'long', day: 'numeric' });
+  });
+
+  eleventyConfig.addFilter('alphabetize', (collectionArray) => {
+    return collectionArray.sort((a, b) => {
+      return a.data.title < b.data.title ? -1 : 1;
+    });
+  });
 
   return {
     dir: {
@@ -17,5 +32,5 @@ module.exports = function (eleventyConfig) {
       output: 'public',
       includes: '_includes',
     },
-  }
-}
+  };
+};
